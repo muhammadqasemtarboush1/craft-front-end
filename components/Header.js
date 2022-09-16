86b5268aa558740af39e29f0423cd3ac4c82e582
essa eayd abu kheir <esaaeyad22@gmail.com>
2022-09-15 19:33:09 -0700

    v1

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,16 @@
 
 export default function Header(){
     return (
-        <header className='flex justify-between bg-slate-500 p-3 w-screen'>
-            <h1 className="text-2xl font-semibold">
-                Crafts
-            </h1>
-            <div className="mr-90 h-7 rounded">
-                <button className="float-right text-black font-bold bg-gray-300 rounded w-20 h-full">
+        <header className='flex justify-between items-center bg-stone-700 p-3 w-screen'>
+            <div className="flex flex-row items-center ">
+            <img class="shadow-md bg-orange-500 rounded-full  p-3 "src="https://thumbs.dreamstime.com/z/illustration-drawing-art-hand-tools-logo-white-background-hand-tools-logo-182124077.jpg" alt="imag" width="7% " height="7%" />
+            <h1 class='text-4xl pl-50 text-orange-500 ml-3'>Craftsmen</h1>
+            </div>
+            <div className="mr-90 h-7 rounded flex flex-row">
+                <button className=" float-right text-white font-bold bg-orange-500 rounded w-20 h-full hover:text-stone-400  ">
                     Log in
                 </button>
-                <button className="float-right text-black mr-2 font-bold bg-gray-300 rounded w-20 h-full">
+                <button className=" ml-3 float-right text-white mr-2 font-bold bg-orange-500 rounded w-20 h-full hover:text-stone-400">
                     Sign up
                 </button>
             </div>
---
+++ components/login.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+
+export default function LoginForm() {
+    return (
+<>
+
+  <div class="flex flex-col justify-center items-center bg-gray-100" >
+    <form class=" flex-auto px-4 my -32 max -w -3xl mx -auto space-y-6 " action="/send-data-here" method="post">
+    
+    <label for="name">Username:</label>
+    <input class="border border - gray - 400 block py - 2 px - 4 w - full rounded focus : outline - none" type="text" id="name" name="name" required />
+    <br/>
+    
+    <label for="pswrd">Password:</label>
+    <input
+    class="border border - gray - 400 block py - 2 px - 4 w - full rounded focus : outline - none"
+    type="password"
+    id="pswrd"
+    name="pswrd"
+    pattern="[a-z0-9]{1,15}"
+    title="Password should be digits (0 to 9) or alphabets (a to z)."
+    />
+    <br/>
+    <div class="flex flex-col">
+
+        <div class="basis-1/2">
+        <input  type="radio" name="As a user" value="linus" checked=""/>
+        <label for="User"> As a user</label>
+        </div>
+        
+        <div class="basis-1/2">
+        <input  type="radio" name="As a craftsmen " value="linus" checked=""/>
+        <label for="craftsmen"> As a craftsmen</label>
+
+        </div>
+    </div>
+    
+    <div class="relative h-32 w-32 ...">
+    <button className="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300 w-full p-2 font-medium text-white uppercase bg-gradient-to-b from-gray-700 to-green-600 md:p-4 rounded-3xl hover:bg-white">login</button>
+    </div>
+    
+  
+</form> 
+</div>
+</>
+    );
+  };
