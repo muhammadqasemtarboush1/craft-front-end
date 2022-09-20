682bfb55a74ce933d79290cc2593a4f0591a64ce
Laila Nouman <lailaibraheem@hotmail.com>
2022-09-20 17:07:45 +0300

    new edits

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -12,12 +12,9 @@ export default function LoginForm() {
     const { login } = useContext(AuthContext);
 
     async function handleSubmit(e) { 
-        
         e.preventDefault();
-        try{
 
-        
-       
+        try{
         await login({
             email,
             password
@@ -27,29 +24,25 @@ export default function LoginForm() {
     } catch(e){
         console.log(e)
     }
-       
     }
 
     function handleemail(e) {
         setemail(e.target.value);
     }
+
     function handleUserPassword(e) {
         setPassword(e.target.value)
     }
     
-
-
     return (
         
 <>
 
   <div class="flex flex-col justify-center items-center bg-gray-100" >
     <form class=" flex-auto px-4 my -32 max -w -3xl mx -auto space-y-6 " action="/send-data-here" method="post">
-    
     <label for="name">email:</label>
     <input onChange={(e)=>handleemail(e)} class="border border - gray - 400 block py - 2 px - 4 w - full rounded focus : outline - none" type="text" id="name" name="name" required />
     <br/>
-    
     <label for="pswrd">Password:</label>
     <input
     onChange={(e)=>handleUserPassword(e)}
@@ -58,10 +51,9 @@ export default function LoginForm() {
     id="pswrd"
     name="pswrd"
     pattern="[a-z0-9]{1,15}"
-    title="Password should be digits (0 to 9) or alphabets (a to z)."
-    />
+    title="Password should be digits (0 to 9) or alphabets (a to z)."/>
     <br/>
-    <div class="flex flex-col">
+    {/* <div class="flex flex-col">
 
         <div class="basis-1/2">
         <input  type="radio" name="As a user" value="linus" checked=""/>
@@ -73,13 +65,12 @@ export default function LoginForm() {
         <label for="craftsmen"> As a craftsmen</label>
 
         </div>
-    </div>
+    </div> */}
     
     <div class="relative h-32 w-32 ...">
     <button onClick={(e)=>handleSubmit(e)} className="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300 w-full p-2 font-medium text-white uppercase bg-gradient-to-b from-gray-700 to-green-600 md:p-4 rounded-3xl hover:bg-white">login</button>
     </div>
     
-  
 </form> 
 </div>
 </>
--- components/Navigation.js
+++ components/Navigation.js
@@ -4,25 +4,25 @@ export default function Navigation(){
     return(
         <nav className='shadow-md box-content h-7 border-none w-screen py-2 bg-orange-400 text-xl font-bold text-center'>
         <Link href="/">
-            <a className="h-20 w-20 p-2 border-none py-1 px-3 text-white hover:text-orange-300" target="_blank"> Home </a>
+            <a className="h-20 w-20 p-2 border-none py-1 px-3 text-white hover:text-orange-300"> Home </a>
         </Link>  
         &nbsp; &nbsp;   
         <Link href="/About">
-            <a className="h-20 w-20 p-2 border-none py-1 px-3 text-white hover:text-orange-300" target="_blank"> About </a>
+            <a className="h-20 w-20 p-2 border-none py-1 px-3 text-white hover:text-orange-300"> About </a>
         </Link> 
         &nbsp; &nbsp;  
         <Link href="/Craftsmen">
-            <a className="h-20 w-20 p-2 border-none py-1 px-3 text-white hover:text-orange-300" target="_blank"> Craftsmen </a>
+            <a className="h-20 w-20 p-2 border-none py-1 px-3 text-white hover:text-orange-300"> Craftsmen </a>
         </Link> 
         &nbsp; &nbsp;
         <Link href="/Products">
-            <a className="h-20 w-20 p-2 border-none py-1 px-3 text-white hover:text-orange-300" target="_blank"> Products </a>
+            <a className="h-20 w-20 p-2 border-none py-1 px-3 text-white hover:text-orange-300"> Products </a>
         </Link> 
         &nbsp; &nbsp;
         <Link href="/Events">
-            <a className="h-20 w-20 p-2 border-none py-1 px-3 text-white  hover:text-orange-300" target="_blank"> Events </a>
+            <a className="h-20 w-20 p-2 border-none py-1 px-3 text-white  hover:text-orange-300"> Events </a>
         </Link> 
-        &nbsp; &nbsp;     
+        &nbsp; &nbsp;   
   </nav>
     )
 }
